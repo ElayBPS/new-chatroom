ae74d15faee022134acdedaa3cc5a42533d26fbf
ElayBPS <wasavmada@gmail.com>
2019-04-30 16:02:02 +0100

    Better than whatsapp

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,30 @@ const firebase = require('firebase-admin');
 firebase.initializeApp();
 exports.sendNotification = functions.firestore.document("messages/{messageId}").onCreate(data => {
     const message = data.data()
+    let text
+    if (message.type === 'image') {
+        text = '📷 Photo'
+    }
+    else if (message.type === 'video') {
+        text = '🎥 Vidéo'
+    }
+    else if (message.type === 'file') {
+        text = '📄 Document'
+    }
+    else if (message.type === 'audio') {
+        text = '🎤 Audio'
+    }
+    else {
+        text = message.text
+    }
     // Retrouver la liste des utilisateurs depuis la database.
     return firebase.firestore().collection("users").get().then(users => {
         return Promise.all(users.docs.map(user => {
             const userData = user.data()
             var notification = {
                 data: {
-                    title: `${message.username} vous a écrit :`,
-                    message: message.text
+                    title: `${message.username}`,
+                    message: text
                 },
                 token: userData.fcm_token
             }
